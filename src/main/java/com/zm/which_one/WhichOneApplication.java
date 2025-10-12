465086c71426dcccee755160a4b5334894ff3412
zmmarina <zmarina.hm@gmail.com>
2025-10-12 20:48:59 -0300

    add spring and dependencies

---
+++ src/main/java/com/zm/which_one/WhichOneApplication.java
@@ -0,0 +1,13 @@
+package com.zm.which_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhichOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhichOneApplication.class, args);
+	}
+
+}
