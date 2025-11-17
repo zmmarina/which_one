e517c41affa9d9cc4cfb43c7890df257e62211f8
zmmarina <zmarina.hm@gmail.com>
2025-11-16 23:57:51 -0300

    fixing dropdown

--- frontend/which-one-front/src/app/translate-loader.ts
+++ frontend/which-one-front/src/app/translate-loader.ts
@@ -7,6 +7,6 @@ export class CustomTranslateLoader implements TranslateLoader {
   constructor(private http: HttpClient) {}
 
   getTranslation(lang: string): Observable<any> {
-    return this.http.get(`./assets/i18n/${lang}.json`);
+    return this.http.get(`/assets/i18n/${lang}.json`);
   }
 }
