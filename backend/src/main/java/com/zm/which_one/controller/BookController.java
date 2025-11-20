af0f4d6e6e8dbe2c42798be12053c511a376fc83
zmmarina <zmarina.hm@gmail.com>
2025-11-20 00:21:49 -0300

    adjust controller

--- backend/src/main/java/com/zm/which_one/controller/BookController.java
+++ backend/src/main/java/com/zm/which_one/controller/BookController.java
@@ -4,11 +4,9 @@ import com.zm.which_one.model.Book;
 import com.zm.which_one.service.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4300")
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
